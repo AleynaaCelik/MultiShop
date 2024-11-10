087e5bd54ccdc6d932f6e9b34474ffba9ada4f4f
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-10 23:16:33 +0300

    OrderDetailsController Created

--- Services/Order/Presantation/MultiShop.Order.WebApi/Controllers/OrderDetailsController.cs
+++ Services/Order/Presantation/MultiShop.Order.WebApi/Controllers/OrderDetailsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
 
 namespace MultiShop.Order.WebApi.Controllers
 {
@@ -7,5 +8,24 @@ namespace MultiShop.Order.WebApi.Controllers
     [ApiController]
     public class OrderDetailsController : ControllerBase
     {
+        private readonly GetOrderDetailQueryHandler _getOrderDetailQueryHandler;
+        private readonly GetOrderDetailByIdQueryHandler _getOrderDetailByIdQueryHandler;
+        private readonly CreateOrderDetailCommandHandler _createOrderDetailCommandHandler;
+        private readonly RemoveOrderDetailQueryHandler _removeOrderDetailCommandHandler;
+        private readonly IUpdateOrderDetailCommandHandler _updateOrderDetailCommandHandler;
+
+        public OrderDetailsController(
+            IGetOrderDetailQueryHandler getOrderDetailQueryHandler,
+        IGetOrderDetailByIdQueryHandler getOrderDetailByIdQueryHandler,
+            ICreateOrderDetailCommandHandler createOrderDetailCommandHandler,
+            IRemoveOrderDetailCommandHandler removeOrderDetailCommandHandler,
+            IUpdateOrderDetailCommandHandler updateOrderDetailCommandHandler)
+        {
+            _getOrderDetailQueryHandler = getOrderDetailQueryHandler;
+            _getOrderDetailByIdQueryHandler = getOrderDetailByIdQueryHandler;
+            _createOrderDetailCommandHandler = createOrderDetailCommandHandler;
+            _removeOrderDetailCommandHandler = removeOrderDetailCommandHandler;
+            _updateOrderDetailCommandHandler = updateOrderDetailCommandHandler;
+        }
     }
 }
