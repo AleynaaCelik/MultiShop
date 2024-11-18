08c957d97f513de518b4fb77e1954a68329a1e69
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-18 23:39:38 +0300

    BasketService Created

---
+++ MultiShop.Basket/Services/BasketService.cs
@@ -0,0 +1,49 @@
+﻿using MultiShop.Basket.Dtos;
+
+namespace MultiShop.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        // Sepeti silme işlemi
+        public async Task DeleteBasketAsync(string userId)
+        {
+            await _redisService.GetDb().KeyDeleteAsync(userId);
+        }
+
+        // Sepeti alma işlemi
+        public async Task<BasketTotalDto> GetBasketAsync(string userId)
+        {
+            try
+            {
+                // Redis'ten sepet verisini al
+                var existBasket = await _redisService.GetDb().StringGetAsync(userId);
+
+                // Sepet verisi varsa, deserialize et ve döndür
+                if (!string.IsNullOrEmpty(existBasket))
+                {
+                    return JsonConvert.DeserializeObject<BasketTotalDto>(existBasket);
+                }
+
+                // Eğer veri yoksa null döndür
+                return null;
+            }
+            catch (Exception)
+            {
+                throw; // Hata varsa dışarı fırlat
+            }
+        }
+
+        // Sepeti kaydetme işlemi
+        public async Task SaveBasketAsync(BasketTotalDto basketTotalDto)
+        {
+            // Sepet verisini serialize edip Redis'e kaydet
+            await _redisService.GetDb().StringSetAsync(basketTotalDto.UserId, JsonConvert.SerializeObject(basketTotalDto));
+        }
+}
