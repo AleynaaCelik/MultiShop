5f8ef1ed7f6365d74f561aca4bf8407b16a8dd60
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-19 23:18:40 +0300

    JwtBearer implemented

--- MultiShop.Comment/Program.cs
+++ MultiShop.Comment/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MultiShop.Comment.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.RequireHttpsMetadata = false;
+    opt.Audience = "ResourceComment";
+});
+
+
+builder.Services.AddDbContext<CommentContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseAuthentication();
 
 app.UseHttpsRedirection();
 
