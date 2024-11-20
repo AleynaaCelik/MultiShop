dff05c62b80b00352ebe11c57e64d49bc24fb395
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-20 23:24:51 +0300

    MessageContext Created

---
+++ Services/Messages/MultiShop.Message/DAL/Context/MessageContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MultiShop.Message.DAL.Context
+{
+    public class MessageContext
+    {
+        public MessageContext(DbContextOptions<MessageContext> options) : base(options)
+        {
+
+        }
+        public DbSet<UserMessage> UserMessages { get; set; }
+    }
+}
